365dee0be62882908a68c27fcce7ccb10a24926f
Romanbek Abdukarimov <romanbek1988@gmail.com>
2021-11-01 21:10:10 +0600

    1-2-tapshyrma

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:tapshyrma1_2/BirinchiBarak.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BirinchiBarak(),
+    );
+  }
+}
